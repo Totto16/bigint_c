1c065cc9ec47feca3d89cefbe2cdad9f04df081e
Totto16 <tobiausgais@gmail.com>
2025-09-16 18:12:48 +0200

    Ready! Set! Go!

---
+++ src/public/bigint_c.h
@@ -0,0 +1,16 @@
+
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../lib/lib.h"
+#include "../parser/fonts.h"
+#include "../parser/parser.h"
+#include "../parser/validate.h"
+
+#ifdef __cplusplus
+}
+#endif
